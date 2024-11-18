b3fae18491dd74563212cb23a1078332c6148ccd
jcodex <j.okorie2@gmail.com>
2024-11-18 18:20:09 +0100

    Fix on all coding Problems

---
+++ backend/src/modules/students/students-controller.js
@@ -0,0 +1,41 @@
+const asyncHandler = require("express-async-handler");
+const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+
+const handleGetAllStudents = asyncHandler(async (req, res) => {
+    const students = await getAllStudents(req.query);
+    res.json({ students });
+});
+
+const handleAddStudent = asyncHandler(async (req, res) => {
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
+});
+
+const handleUpdateStudent = asyncHandler(async (req, res) => {
+    const { id: userId } = req.params;
+    const payload = req.body;
+    const message = await updateStudent({ ...payload, userId });
+    res.json(message);
+
+});
+
+const handleGetStudentDetail = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
+});
+
+const handleStudentStatus = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const studentStatus = await setStudentStatus(id);
+    res.json(studentStatus);
+});
+
+module.exports = {
+    handleGetAllStudents,
+    handleGetStudentDetail,
+    handleAddStudent,
+    handleStudentStatus,
+    handleUpdateStudent,
+};
